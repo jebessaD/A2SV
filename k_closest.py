9d82c4999fb117109dd38e98aa20af74b5e3ad57
jebessad <jebessadejene2021@gmail.com>
2022-05-13 13:00:27 +0300

    remove white spaces

--- k_closest.py
+++ k_closest.py
@@ -1,11 +1,8 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-
         myDict={}
         for index,point in enumerate(points):
             distance=point[0]**2 + point[1]**2
-            
             myDict[(distance,index)]=point
         sorted_values=dict(sorted(myDict.items(), key=lambda item: item[0]))
-
         return(list(sorted_values.values())[:k])
