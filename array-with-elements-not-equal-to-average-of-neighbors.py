63fa58664f5a1ebb6a7f898d1ec44be6589051eb
jebessad <jebessadejene2021@gmail.com>
2022-05-13 12:59:59 +0300

    remove white spaces

--- array-with-elements-not-equal-to-average-of-neighbors.py
+++ array-with-elements-not-equal-to-average-of-neighbors.py
@@ -6,9 +6,7 @@ class Solution:
         for half in range(length//2):
             newArray[2*half+1]=nums[half]
             newArray[2*half]=nums[half-(length//2)]
-
             if (length%2!=0):
-                newArray[length-1]=nums[length//2]
-                
+                newArray[length-1]=nums[length//2]    
         return(newArray)
         
