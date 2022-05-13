68f55a29700909e5f419acd8a1e64ad6a2ade452
jebessad <jebessadejene2021@gmail.com>
2022-05-13 10:43:45 +0300

    array-with-elements-not-equal-to-average-of-neighbors

---
+++ array-with-elements-not-equal-to-average-of-neighbors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        length=len(nums)
+        newArray=[0]*length
+        for half in range(length//2):
+            newArray[2*half+1]=nums[half]
+            newArray[2*half]=nums[half-(length//2)]
+
+            if (length%2!=0):
+                newArray[length-1]=nums[length//2]
+                
+        return(newArray)
+        
