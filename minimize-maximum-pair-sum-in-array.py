7122c7bada57b01d63649857a25cf94d24399acd
jebessad <jebessadejene2021@gmail.com>
2022-05-21 18:32:27 +0300

    minimize-maximum-pair-sum-in-array

---
+++ minimize-maximum-pair-sum-in-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        size=len(nums)
+        arr=[]
+        for i in range(size//2):
+            a=nums[i]+nums[size-1-i]
+            arr.append(a)
+        return(max(arr))
+        
