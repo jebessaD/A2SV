091530da25c448f27e035132e89e83caa4226a68
jebessad <jebessadejene2021@gmail.com>
2022-05-23 10:08:21 +0300

    valid-parentheses

---
+++ valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        data={'(': ')', '{': '}', '[':']'}
+        stack=[]
+        for brac in s:
+            if brac in data.keys():
+                stack.append(brac)
+
+            else:
+                if len(stack)>0:
+                    a= stack.pop()
+                    if data[a] != brac:
+                        return False
+                else:
+                    return False
+        if len(stack)==0:
+            return True
