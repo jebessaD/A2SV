d7081795ebcf6ef2f5871d8e304bf754d3568983
jebessad <jebessadejene2021@gmail.com>
2022-09-18 21:34:05 +0300

    using stack and iteration

---
+++ decode-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        element,sub,stack=0,"",[]
+        
+        for i in s:
+            while(i=="]"):
+                a=stack.pop()
+                if(a=="["):
+                    num=stack.pop()
+                    stack.append(num*sub)
+                    sub=""
+                    break
+                else:
+                    sub=a + sub
+                    
+            if i.isnumeric() :
+                element=element * 10 + int(i)
+            elif(i=="["):
+                stack.append(element)
+                stack.append(i)
+                element=0
+            elif(i !="]"):
+                stack.append(i)
+                
+        return(''.join(stack))
+                
