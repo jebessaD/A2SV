73d6e2f8dc316e7849f75f53bb0af264ac76a621
jebessad <jebessadejene2021@gmail.com>
2022-09-30 01:35:02 +0300

    prefix sum

---
+++ find-pivot-index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total=sum(nums)
+        leftsum=0
+        for i,n in enumerate(nums):
+            if(leftsum==total - leftsum - n): #right sum is total - leftsum - n
+                return i  
+            leftsum+=n
+        return -1
