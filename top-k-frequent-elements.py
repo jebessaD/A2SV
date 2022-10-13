275a41f8eaa91a461eb62b5429debefec5c00094
jebessad <jebessadejene2021@gmail.com>
2022-10-14 00:14:12 +0300

    optimization sorting

--- top-k-frequent-elements.py
+++ top-k-frequent-elements.py
@@ -1,3 +1,4 @@
+# https://github.com/jebessaD/A2SV/blob/main/top-k-frequent-elements.py
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         mydict={}
@@ -14,3 +15,13 @@ class Solution:
         for num in sorted_dict.keys():
             nums_set.append(num)
         return(nums_set[:k])
+
+
+# solution 2
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        c=Counter(nums).most_common()
+        return list(map(lambda x:x[0],c[:k]))
+        
+
+        
