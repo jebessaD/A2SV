abbf02803091fd4491086b8600b55da7711ecbf7
jebessad <jebessadejene2021@gmail.com>
2022-10-15 22:15:37 +0300

    prefix sum

--- car-pooling.py
+++ car-pooling.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/car-pooling/
+
+
+#first solution using brute force
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         trips.sort(key=lambda x:x[2],reverse=True)
@@ -10,3 +13,20 @@ class Solution:
                 if(ans[n]>capacity):
                     return False
         return True
+
+# second and optimal solution using prefix sum
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key=lambda x:x[2], reverse=True) # sort by index ==2 to get last destination
+        car=[0]*(trips[0][2] + 1)  #get last destination for second index
+
+        for n,i,j in trips:   #add n number of person at index i and drop person at index of j
+            car[i] +=n
+            car[j] -=n
+            
+        total=0
+        for person in car:
+            total+=person
+            if total>capacity: return False
+        return True
+        
