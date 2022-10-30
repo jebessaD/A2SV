405ca864249b73428635c5e0d2fc8c905d07e6f3
jebessad <jebessadejene2021@gmail.com>
2022-10-30 10:24:22 +0300

    heap medium

--- kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array.py
@@ -1,3 +1,14 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return sorted(nums)[-k]
+
+
+#solution 2 using heap
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums=list(map(lambda x:-x,nums))
+        heapq.heapify(nums)
+        for i in range(k-1):
+            heapq.heappop(nums)
+        return -heapq.heappop(nums)
+        
