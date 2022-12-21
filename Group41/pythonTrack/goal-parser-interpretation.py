422209389b835fdbbe395ea988b0aa504a4b183c
jebessad <jebessadejene2021@gmail.com>
2022-12-21 21:23:08 +0300

    goal-parser-interpretation

---
+++ Group41/pythonTrack/goal-parser-interpretation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        ans=""
+        i=0
+        while i <len(command):
+            if command[i] == "G":
+                ans+="G"
+            else:
+                if command[i+1]==")":
+                    ans+="o"
+                    i+=1
+                else:
+                    ans+="al"
+                    i+=3
+            i+=1
+        return ans
+            
