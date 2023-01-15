42c04ecc80c17a91542aa8c4486ec38d4af7c5b9
jebessad <jebessadejene2021@gmail.com>
2023-01-15 23:35:12 +0300

    using two pointers and tilde operator

--- Group41/reverse-string.py
+++ Group41/reverse-string.py
@@ -10,3 +10,13 @@ class Solution:
             s[left],s[right] = s[right],s[left]
             left += 1
             right -= 1
+
+#second solution tilde (~) bitwise operator
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead. 
+        2 pointers
+        """
+        for ind in range(len(s)//2):
+            s[ind],s[~ind]=s[~ind],s[ind]
