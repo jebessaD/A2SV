6a151c8bd6588ef9789c7286e43376869d8fa73f
jebessad <jebessadejene2021@gmail.com>
2023-02-14 23:28:19 +0300

    two pointers

---
+++ Group41/progress/codeforces/B - Number of Smaller.py
@@ -0,0 +1,15 @@
+input()
+arr1=list(map(int,input().split(" ")))
+arr2=list(map(int,input().split(" ")))
+ptr1 = ptr2 = count = 0
+ans=[]
+while ptr2 < len(arr2):
+ 
+ if ptr1 < len(arr1) and arr1[ptr1] < arr2[ptr2]:
+  count+=1
+  ptr1+=1
+ else:
+  ans.append(count)
+  ptr2+=1
+print(*ans)
+  
