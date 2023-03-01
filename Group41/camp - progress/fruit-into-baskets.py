ad2c675b402b954b237e375ee8003fe3bb4e3807
jebessad <jebessadejene2021@gmail.com>
2023-03-01 08:34:19 +0300

    prefix sum

---
+++ Group41/camp - progress/fruit-into-baskets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = {}
+        ans = 0
+        left = 0
+        for right in range(len(fruits)):
+            basket[fruits[right]] = basket.get(fruits[right],0) + 1
+            while len(basket) > 2:
+                basket[ fruits[left] ] -= 1
+                if basket[fruits[left]] == 0:
+                    basket.pop(fruits[left])
+                left += 1
+
+            ans = max(ans , right - left + 1)
+            
+        return ans
