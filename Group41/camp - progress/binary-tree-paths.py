b646ef87de7cfbe3cc56072fa4f9061e05fa92c1
jebessad <jebessadejene2021@gmail.com>
2023-03-21 21:52:00 +0300

    backtracking modified

--- Group41/camp - progress/binary-tree-paths.py
+++ Group41/camp - progress/binary-tree-paths.py
@@ -4,22 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         ans = []
-        def binaryTree(root, path = []):
-            path.append(str(root.val))
+        curr = [str(root.val)]
+        def findLeaf(root,curr,ans):
             if not root.left and not root.right:
-                ans.append("->".join(path))
-                return
-            
-            if root.left:
-                binaryTree(root.left, path)
-                path.pop()
-                
+                ans.append("->".join(map(str,curr)))
+                return 
+
+            if root.left :
+                curr.append(root.left.val)
+                findLeaf(root.left,curr,ans)
+                curr.pop()
+
             if root.right:
-                binaryTree(root.right, path)
-                path.pop()
-        binaryTree(root, [])
+                curr.append(root.right.val)
+                findLeaf(root.right,curr,ans)
+                curr.pop()
+
+        findLeaf(root,curr,ans)
         return ans
+
+
+
+        
