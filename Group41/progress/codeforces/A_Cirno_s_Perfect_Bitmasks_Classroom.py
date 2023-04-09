879b25c6d0b1a84b99530958413420c0cc8977cb
jebessad <jebessadejene2021@gmail.com>
2023-04-09 23:16:33 +0300

    bit manipulation

--- Group41/progress/codeforces/A_Cirno_s_Perfect_Bitmasks_Classroom.py
+++ Group41/progress/codeforces/A_Cirno_s_Perfect_Bitmasks_Classroom.py
@@ -2,15 +2,14 @@ n = int(input())
 
 for _ in range(n):
     num = num2 = int(input())
-    a = num
-    
     and_shift = 0
-    xor_shift = 0
+
     while num % 2 == 0:
         num = num >> 1
         and_shift += 1
 
     ans = 1 << and_shift
+    
     curr_xor = ans ^ num2
 
     if not curr_xor:
