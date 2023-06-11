884e410c5e047ca214d83b8b6ea59426c3210849
jebessad <jebessadejene2021@gmail.com>
2023-06-11 18:03:24 +0300

    topographical sort ninja

---
+++ Group41/progress/parallel-course.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from collections import deque
+def parallelCourses(n, prerequisites):
+    # Write your code here.
+    graph = defaultdict(list)
+    indegrees = defaultdict(int)
+    for pre,post in prerequisites:
+        graph[pre].append(post)
+        indegrees[post] += 1
+
+    # print(graph,indegrees)
+    queue = deque()
+    visited = set()
+    for i in range(1,n+1):
+        if i not in indegrees:
+            queue.append((i,1))
+            visited.add(i)
+    max_level = -1
+
+    while queue:
+        parent, level = queue.popleft()
+        
+        max_level = max(max_level,level)
+        for child in graph[parent]:
+            indegrees[child] -= 1
+            if not indegrees[child]:
+                queue.append((child,level+1))
+                visited.add(child)
+
+    if len(visited) != n:
+        return -1
+    return max_level
