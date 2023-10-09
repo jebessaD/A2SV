b3aa4420522cd0974a5192116af57609f7179c06
Jebessa Dejene <89038777+jebessaD@users.noreply.github.com>
2023-10-09 10:53:25 +0300

    Added solution for Uncrossed Lines

---
+++ tree/main/Group41/progress/uncrossed-lines.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        #n
+        dp =  [[0]*(len(nums2) + 1) for _ in range(len(nums1)+1)]
+        
+        for i in range(1,len(nums1)+1):
+            for j in range(1,len(nums2)+1):
+                
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+                    
+        return dp[len(nums1)][len(nums2)]
+        
