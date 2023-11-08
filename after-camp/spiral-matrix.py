0d5a55f5769739baf98e98896f36eae7d08e13a1
Jebessa Dejene <89038777+jebessaD@users.noreply.github.com>
2023-11-08 10:18:02 +0300

    Added solution for Spiral Matrix

---
+++ after-camp/spiral-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        def rotateCounter(matrix):
+            rotatedClockwise = list(zip(*matrix))
+            return list(reversed(rotatedClockwise))
+
+        ans = []
+        while len(matrix) > 0:
+            poped = matrix.pop(0)
+            ans.extend(poped)
+            matrix = rotateCounter(matrix)
+
+        return ans
+
+
+
+
+        
+        
+        
